9d91a33267acfde2bf3462ee5dbefbca24786ade
Matt <hello@mattnull.com>
2014-01-15 12:11:11 -0700

    removing unused classes

--- public/js/templates/templates.js
+++ public/js/templates/templates.js
@@ -8,7 +8,7 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
 
   buffer += "<h1>Hi! This is a Handlebars template!! ";
   if (stack1 = helpers.data) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
-  else { stack1 = depth0.data; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  else { stack1 = (depth0 && depth0.data); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
   buffer += escapeExpression(stack1)
     + "</h1>";
   return buffer;
